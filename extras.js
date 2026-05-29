1bf7c9d207a5cdf5392f214a8a2a79569cc4b3d4
Alexlex2205 <iruishikawa@gmail.com>
2026-05-29 15:14:57 -0600

    ora

--- extras.js
+++
@@ -1,8 +0,0 @@
-let current = 0;
-const slides = document.querySelectorAll('.slide');
-const track = document.getElementById('track');
-
-setInterval(() => {
-  current = (current + 1) % slides.length;
-  track.style.transform = `translateX(-${current * 100}%)`;
-}, 3000);
